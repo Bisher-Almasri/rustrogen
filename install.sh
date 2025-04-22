9f55bb4f3430507ee71ead8faa1fe19d407caf36
BISHER-AL-MASRI <bbgyttv@gmail.com>
2025-04-22 17:22:03 -0400

    fixed installer

--- install.sh
+++ install.sh
@@ -11,11 +11,33 @@ info() {
 success() {
   echo "[✔] $1"
 }
-
+if [ -d "/Applications/rustrogen.app" ]; then
+  echo "Rustrogen is already installed."
+  echo "Would you like to reinstall it?"
+  select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) rm -rf /Applications/rustrogen.app; break;;
+        No ) exit;;
+    esac
+  done
+fi
 info "Downloading Rustrogen"
 curl -fsSL "$RUSTROGEN_URL" -o "/tmp/rustrogen.app.zip"
 info "Unzipping Rustrogen"
 unzip /tmp/rustrogen.app.zip -d /tmp
 info "Installing Rustrogen"
 mv /tmp/rustrogen.app /Applications
+info "Trusting Rustrogen"
+xattr -rd com.apple.quarantine /Applications/rustrogen.app
+chmod +x /Applications/rustrogen.app/Contents/MacOS/rustrogen
+info "Cleaning up"
+rm /tmp/rustrogen.app.zip
 success "Rustrogen installed successfully!"
+
+info "Would you like to open Rustrogen now?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) open /Applications/rustrogen.app; break;;
+        No ) break;;
+    esac
+done
